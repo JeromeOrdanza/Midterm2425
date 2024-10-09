d21ef6e5613820b562f5cd938e58dbf5d8ed4dc8
JeromeOrdanza <20215871@s.ubaguio.edu>
2024-10-09 13:23:29 +0800

    oct 9

--- Midterm2425/src/app/app.component.ts
+++ Midterm2425/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularMidterm_Marquez';
+  title = 'OrdanzaProject';
 }
